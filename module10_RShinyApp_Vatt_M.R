b134583bc3d9da2e7933f93399d28d64cd78c9d3
mjvatt <56173397+mjvatt@users.noreply.github.com>
2020-01-08 22:16:30 -0500

    Update module10_RShinyApp_Vatt_M.R

--- module10_RShinyApp_Vatt_M.R
+++ module10_RShinyApp_Vatt_M.R
@@ -2,4 +2,50 @@
 ## 19 DEC 2019
 ## Assignment 10: Create an R Shiny App
 
-## Overview ##
+
+#### This is the ui.R script ####
+
+shinyServer(
+  pageWithSidebar(
+    headerPanel("My First Shiny App"),
+    
+    sidebarPanel("Side Bar"), 
+      selectInput("Distribution", "Please Select Distribution Type",
+                  choices = c("Normal", "Exponential")),
+      sliderInput("Sample Size", "Please Select Sample Size: ",
+                  min = 100, max = 5000, value = 1000, step = 100),
+      conditionalPanel(condition = "input.Distribution == 'Normal'",
+                      textInput("Mean", "Please Select the Mean", 10),
+                      textInpur("sd", "Please Select Standard Deviation", 3)),
+    conditionalPanel(condition = "input.Distribution == 'Exponential'",
+                      textInput("Lambda", "Please Select Exponential Lambda: ", 1))
+      ),
+    mainPanel(
+      plotOutput("myPlot")
+    
+    )
+ )
+
+
+#### This is the server.R script ####
+
+shinyServer(
+ function(input, output, session){
+   
+   output$myPlot <- renderPlot({
+     
+     distType <- input$Distribution
+     size <- input$sampleSize
+     
+     if(distType == "Normal"){
+        randomVec <- rnorm(size, mean = as.numeric(input$mean), sd = as.numeric(input$sd))
+     }
+     else {
+        randomVec <- rexp(size, rate = 1 / as.numeric(input$lambda))  
+     }
+     hist(randomVec, col = "orange")
+   })  
+ }
+  
+  
+)  
